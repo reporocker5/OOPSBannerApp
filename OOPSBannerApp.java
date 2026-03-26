d12f35eb2f23eb78dbd798f885c847d38e7c7613
reporocker5 <sl7299@srmist.edu.in>
2026-03-26 15:06:13 +0530

    Render OOPS as Banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,15 @@
+/**
+ * UC3 - Render OOPS as Banner using String.join()
+ * Prints OOPS using 7-line, 9-width grid format using asterisks (*) and spaces( ) to form each letter
+ */
 public class OOPSBannerApp {
-   public static void main(String[] args) {
-        System.out.println("  **  " + "    **  " + "***  " + "  ***   ");
-        System.out.println(" *   * " + " *   * " + "*   *" + "*      *");
-        System.out.println("*     *" + "*     *" + "*   *" + "**      ");
-        System.out.println("*     *" + "*     *" + "***  " + "   ***  ");
-        System.out.println("*     *" + "*     *" + "*    " + "       *");
-        System.out.println(" *   * " + " *   * " + "*    " + "*     **");
-        System.out.println("  **  " + "   **  "  + "*    " + " ***    ");
-   }
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "  *****  ", "  *****  ", " ******* ", " ******* "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **    **", " **      "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **    **", " **      "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " ******* ", " ******* "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **      ", "      ** "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **      ", "      ** "));
+        System.out.println(String.join(" ", "  *****  ", "  *****  ", " **      ", " ******* "));
+    }
 }
